c046db6774ffd6082c3949e3afa6f13c4d951dd7
dirksavage <dirksavage88@gmail.com>
2024-02-08 21:57:59 +0000

    increase default fps param to 60
    Signed-off-by: dirksavage <dirksavage88@gmail.com>

--- ros2_usbcam.sh
+++ ros2_usbcam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting ros2 usb cam driver"
 source install/setup.bash
-ros2 run usb_camera_driver usb_camera_driver_node --ros-args -p image_width:=640 -p image_height:=240 -p fps:=30 -p camera_calibration_file:=file:///workspaces/isaac_ros-dev/ros_ws/ros2_usb_camera/config/camera.yaml
+ros2 run usb_camera_driver usb_camera_driver_node --ros-args -p image_width:=640 -p image_height:=240 -p fps:=60 -p camera_calibration_file:=file:///workspaces/isaac_ros-dev/ros_ws/ros2_usb_camera/config/camera.yaml
 
