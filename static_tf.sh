ed714dca49e4c16bccb4f2a88970b3660b0ddd3c
dirksavage <dirksavage88@gmail.com>
2024-02-08 22:21:13 +0000

    add sensor offset to stereo camera due to placement
    Signed-off-by: dirksavage <dirksavage88@gmail.com>

--- static_tf.sh
+++ static_tf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "starting static transforms"
 source install/setup.bash
-ros2 run tf2_ros static_transform_publisher 0.07 0.05 -0.01 0 0 0 base_link input_base_frame & ros2 run tf2_ros static_transform_publisher -0.07 -0.08 0.01 0 0 0 input_base_frame imu
+ros2 run tf2_ros static_transform_publisher 0.07 0.05 -0.01 0.35 0.35 0 base_link input_base_frame & ros2 run tf2_ros static_transform_publisher -0.07 -0.08 0.01 0 0 0 input_base_frame imu
 
