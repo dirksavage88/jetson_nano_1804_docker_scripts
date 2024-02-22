5d226cc824cbf9c88764773c8ec48909cabe9fe3
dirksavage <dirksavage88@gmail.com>
2024-02-22 18:11:58 +0000

    remove output image dimensions from stereo splitter
    Signed-off-by: dirksavage <dirksavage88@gmail.com>

--- side_x_side.sh
+++ side_x_side.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting side-by-side stereo splitter"
 source install/setup.bash
-ros2 run side_x_side_stereo side_x_side_stereo_node --ros-args -p output_width:=320 -p output_height:=120 -p input_image_topic:=/image_raw -p right_cam_calibration_file:=file:///workspaces/isaac_ros-dev/ros_ws/side_x_side_stereo/config/right.yaml -p left_cam_calibration_file:=file:///workspaces/isaac_ros-dev/ros_ws/side_x_side_stereo/config/left.yaml
+ros2 run side_x_side_stereo side_x_side_stereo_node --ros-args -p input_image_topic:=/image_raw -p right_cam_calibration_file:=file:///workspaces/isaac_ros-dev/ros_ws/side_x_side_stereo/config/right.yaml -p left_cam_calibration_file:=file:///workspaces/isaac_ros-dev/ros_ws/side_x_side_stereo/config/left.yaml
 
