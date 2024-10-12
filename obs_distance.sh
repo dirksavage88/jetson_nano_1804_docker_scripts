c3cc240e4904253a409e6511a4a13b02d0ca5c99
dirksavage <dirksavage88@gmail.com>
2024-10-12 01:08:48 +0000

    switch to cyclonedds rmw
    Signed-off-by: dirksavage <dirksavage88@gmail.com>

---
+++ obs_distance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Starting Obstacle Distance Node"
+# Open up the i2c permissions
+sudo groupadd i2c
+sudo chown :i2c /dev/i2c-0
+sudo chmod g+rw /dev/i2c-0
+sudo usermod -aG i2c $USER
+
+source install/setup.bash
+ros2 run jetson_obs_distance jetson_obs_distance --ros-args -p i2c_bus:=0
